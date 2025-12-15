77041367e33265db923ab558fc6a5ba66ad88784
Isroil2006 <uu64792@gmail.com>
2025-12-15 17:35:56 +0500

    home/academy bag togirlandi

--- src/pages/home/sections/academy.tsx
+++ src/pages/home/sections/academy.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Academy() {
     return (
         <div className="myContainer">
-            <div className=" bg-[url('home-images/academy-images/academy-bg.png')] bg-cover h-[250px] flex  items-center justify-between p-13 gap-4 rounded-[30px]">
+            <div className=" bg-[url('/home-images/academy-images/academy-bg.png')] bg-cover h-[250px] flex  items-center justify-between p-13 gap-4 rounded-[30px]">
                 <div className="flex  items-center justify-center gap-5">
                     <img src="home-images/academy-images/academy-icon.png" alt="" />
                     <p className="font-semibold text-[20px] font-exo">Let's Start With Academy LMS</p>
