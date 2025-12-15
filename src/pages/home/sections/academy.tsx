1b6c8d323d13a9650b1f3778453cad03cdcd237a
Isroil2006 <uu64792@gmail.com>
2025-12-15 17:30:10 +0500

    home/academy bag togirlandi

--- src/pages/home/sections/academy.tsx
+++ src/pages/home/sections/academy.tsx
@@ -3,15 +3,13 @@ import { Button } from "@/components/ui/button";
 export default function Academy() {
     return (
         <div className="myContainer">
-            <div className=" bg-[url('home-images/academy-images/academy-bg.png')] bg-gradient-to-r from-[#C7E6F9] to-[#E8D1FE] bg-cover h-[250px] flex  items-center justify-between p-13 gap-4 rounded-[30px]">
+            <div className=" bg-[url('home-images/academy-images/academy-bg.png')] bg-cover h-[250px] flex  items-center justify-between p-13 gap-4 rounded-[30px]">
                 <div className="flex  items-center justify-center gap-5">
                     <img src="home-images/academy-images/academy-icon.png" alt="" />
-                    <p className="font-semibold text-[20px] font-exo">Let’s Start With Academy LMS</p>
+                    <p className="font-semibold text-[20px] font-exo">Let's Start With Academy LMS</p>
                 </div>
                 <div className="flex  items-center justify-center gap-6 font-jost">
-                    <Button className="bg-transparent hover:bg-transparent border-2 border-[#FF782D] text-[#FF782D] rounded-[50px]">
-                        I'm a student
-                    </Button>
+                    <Button className="bg-transparent hover:bg-transparent border-2 border-[#FF782D] text-[#FF782D] rounded-[50px]">I'm a student</Button>
                     <Button className="myPrimary rounded-[50px] bg-[#FF782D] ">Become an Instructor</Button>
                 </div>
             </div>
